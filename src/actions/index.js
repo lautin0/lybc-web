e766623f228975fac950ab1a6c9ab2a623ca2023
lautin0 <byzantium01@gmail.com>
2020-02-23 17:26:51 +0800

    Revert "- Splitting saga, action"
    This reverts commit 9348350d68526fb4daf759807e0e40afd55d24f4.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,114 @@
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
+
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+
+export const CHECKOUT_REQUEST = 'CHECKOUT_REQUEST'
+export const CHECKOUT_SUCCESS = 'CHECKOUT_SUCCESS'
+export const CHECKOUT_FAILURE = 'CHECKOUT_FAILURE'
+
+export const SAVE_NEWCOMER_REQUEST = 'SAVE_NEWCOMER_REQUEST'
+export const SAVE_NEWCOMER_SUCCESS = 'SAVE_NEWCOMER_SUCCESS'
+export const SAVE_NEWCOMER_FAILURE = 'SAVE_NEWCOMER_FAILURE'
+
+export const FETCH_NEWCOMER_REQUEST = 'FETCH_NEWCOMER_REQUEST'
+export const FETCH_NEWCOMER_SUCCESS = 'FETCH_NEWCOMER_SUCCESS'
+export const FETCH_NEWCOMER_FAILURE = 'FETCH_NEWCOMER_FAILURE'
+
 export const RESET_SYSTEM_ERROR = 'RESET_SYSTEM_ERROR'
 export const RESET_SYS_MESSAGE = 'RESET_SYS_MESSAGE'
 
+export function getAllProducts() {
+    return {
+        type: GET_ALL_PRODUCTS,
+    }
+}
+
+export function receiveProducts(products) {
+    return {
+        type: RECEIVE_PRODUCTS,
+        products: products,
+    }
+}
+
+export function addToCart(productId) {
+    return {
+        type: ADD_TO_CART,
+        productId,
+    }
+}
+
+export function removeFromCart(productId) {
+    return {
+        type: REMOVE_FROM_CART,
+        productId,
+    }
+}
+
+export function checkout() {
+    return {
+        type: CHECKOUT_REQUEST,
+    }
+}
+
+export function checkoutSuccess(cart) {
+    return {
+        type: CHECKOUT_SUCCESS,
+        cart,
+    }
+}
+
+export function checkoutFailure(error) {
+    return {
+        type: CHECKOUT_FAILURE,
+        error,
+    }
+}
+
+export function fetchNewComers(pageSize, page) {
+    return {
+        type: FETCH_NEWCOMER_REQUEST,
+        pageSize,
+        page
+    }
+}
+
+export function fetchNewComersSuccess(newComers) {
+    return {
+        type: FETCH_NEWCOMER_SUCCESS,
+        newComers
+    }
+}
+
+export function fetchNewComersFailure(error) {
+    return {
+        type: FETCH_NEWCOMER_FAILURE,
+        error
+    }
+}
+
+export function saveNewComer(person) {
+    return {
+        type: SAVE_NEWCOMER_REQUEST,
+        person
+    }
+}
+
+export function saveNewComerSuccess(message) {
+    return {
+        type: SAVE_NEWCOMER_SUCCESS,
+        message
+    }
+}
+
+export function saveNewComerFailure(person, error) {
+    return {
+        type: SAVE_NEWCOMER_FAILURE,
+        person,
+        error
+    }
+}
 
 export function resetSysError(){
     return {
@@ -13,6 +121,3 @@ export function resetSysMessage(){
         type: RESET_SYS_MESSAGE,
     }
 }
-
-export * from './newComer'
-export * from './shop'
