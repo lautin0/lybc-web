d43fbf14e69f281f30cd0d4de32235af926097aa
lautin0 <byzantium01@gmail.com>
2020-10-06 14:13:07 +0800

    Update validator.ts

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -13,7 +13,6 @@ const Validators = {
     return value != null && trim(value).length > 0
   },
   NoWhiteSpaceForValue: (targetValue: any, toBe: any) => {
-    console.log(targetValue === toBe)
     return targetValue === toBe ? (value: any) => {
       return value != null && trim(value).length > 0
     } : Validators.Default
