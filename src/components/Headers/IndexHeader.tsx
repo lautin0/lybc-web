c916a9053f7e472aaa4994435787bbccc1392243
lautin0 <byzantium01@gmail.com>
2020-10-09 19:09:52 +0800

    Test commit

--- src/components/Headers/IndexHeader.tsx
+++ src/components/Headers/IndexHeader.tsx
@@ -11,8 +11,8 @@ function IndexHeader() {
   React.useEffect(() => {
     const updateScroll = () => {
       let windowScrollTop = window.pageYOffset / 3;
-      pageHeader.current.style.transform =
-        `translate3d(0,${windowScrollTop}px,0)`;
+      pageHeader.current && (pageHeader.current.style.transform =
+        `translate3d(0,${windowScrollTop}px,0)`);
     };
     window.addEventListener("scroll", updateScroll);
     return function cleanup() {
--- src/views/articles/sub-sharing/Sharing1.tsx
+++ src/views/articles/sub-sharing/Sharing1.tsx
@@ -1,13 +1,89 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { useEffect } from "react";
+import { Container, Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 // react-bootstrap components
 
 // core components
 
 function Sharing1() {
+
+  const [hallelujah, setHallelujah] = useState(false);
+  const [pray, setPray] = useState(false);
+
+  const setReaction = (reaction: string) => {
+    if (reaction === 'hallelujah') {
+      setHallelujah(!hallelujah)
+      setPray(false)
+    } else if (reaction === 'pray') {
+      setHallelujah(false)
+      setPray(!pray)
+    }
+  }
+
+  useEffect(() => {
+    document.querySelector('.scroll-animations .animated')?.classList.remove("animate__fadeInLeft");
+  }, [])
+
+  useEffect(() => {
+    // Check if element is scrolled into footer
+    const isScrolledIntoFooter = () => {
+      var docViewTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+      var docViewBottom = docViewTop + window.innerHeight
+
+      let el: any = document.querySelector("div#reaction-bar");
+
+      if (el == null)
+        return false
+      return docViewBottom > el.getBoundingClientRect().top + window.pageYOffset
+    }
+
+    // If element is scrolled into view, fade it in
+    const handleScroll = () => {
+      if (isScrolledIntoFooter() === true) {
+        document.querySelector('.scroll-animations .animated')?.classList.add("animate__fadeOut");
+        document.querySelector('.scroll-animations .animated')?.classList.remove("animate__fadeIn");
+      } else {
+        document.querySelector('.scroll-animations .animated')?.classList.add("animate__fadeIn");
+        document.querySelector('.scroll-animations .animated')?.classList.remove("animate__fadeOut");
+      }
+    }
+
+    window.addEventListener("scroll", (e: any) => {
+      handleScroll();
+    })
+
+    return function cleanup() {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  })
+
+  const renderTooltip = (props: any, text: string) => (
+    <Tooltip {...props}>{text}</Tooltip>
+  );
+
   return (
-    <Container className="card" style={{ paddingTop: 90, paddingBottom: 90, borderRadius: '.5rem', marginBottom: 100 }}>
+    <Container style={{ paddingTop: 90, paddingBottom: 90, borderRadius: '.5rem', marginBottom: 100 }}>
+      <Row className="text-left d-none d-lg-block scroll-animations" style={{ position: "sticky", top: '40vh' }}>
+        <div style={{ position: "absolute", marginTop: 80 }} className="animated animate__animated animate__fast">
+          <OverlayTrigger placement="auto" overlay={(props: any) => renderTooltip(props, '哈利路亞')}>
+            <div className="my-3" style={{ cursor: 'pointer' }} onClick={() => setReaction('hallelujah')}>
+              <div style={{ display: 'inline' }}>
+                <i className={`fas fa-hanukiah reaction ${hallelujah ? "reacted" : ""}`}></i>
+              </div>
+              <span style={{ fontSize: 24 }} className="m-1">0</span>
+            </div>
+          </OverlayTrigger>
+          <OverlayTrigger placement="auto" overlay={(props: any) => renderTooltip(props, '記念')}>
+            <div className="my-3" style={{ cursor: 'pointer' }} onClick={() => setReaction('pray')}>
+              <div style={{ display: 'inline' }}>
+                <i className={`fas fa-praying-hands reaction ${pray ? "reacted" : ""}`}></i>
+              </div>
+              <span style={{ fontSize: 24 }} className="m-1">0</span>
+            </div>
+          </OverlayTrigger>
+        </div>
+      </Row>
       <Row className="text-left" style={{ alignItems: 'baseline' }}>
         <Col lg={{ offset: 4 }}><h3><strong>疫情中的信仰 - 神的應許和人的盼望</strong></h3></Col>
         <Col className="text-right pr-5" lg={12}><h5 style={{ color: 'gray' }}>古偉健弟兄 2020年4月5日</h5></Col>
@@ -30,8 +106,28 @@ function Sharing1() {
           <p>正如 路加福音 13:1-9 "正當那時、有人將彼拉多使加利利人的血攙雜在他們祭物中的事、告訴耶穌。 耶穌說、你們 以 為這些加利利人比眾加利利人更有罪、所以受這害麼。 我告訴你們、不是的.你們若不悔 改、都 要如此滅亡。 從前西羅亞樓倒塌了、壓死十八個人.你們以為那些人比一切住在耶路 撒冷的人更 有罪麼。 我告訴你們、不是的.你們若不悔改、都要如此滅亡。 於是用比喻說、 一個人有一棵無 花果樹、栽在葡萄園裡.他來到樹前找果子、卻找不著。 就對管園的說、看 哪、我這三年、來到 這無花果樹前找果子、竟找不著、把他砍了罷.何必白佔地土呢。管園 的說、主阿、今年且留 著、等我周圍掘開土、加上糞.以後若結果子便罷.不然再把他砍 了。"</p>
           <p>信徒真正的盼望是:不論是生是死，靠着基督救贖脱離罪，能在神的面前站立得着，不是白佔 地土的人!</p>
           <p>互勉!</p>
-          <hr/>
+          <hr />
           <p>牧者建議信徒進深學習:<br /> 查考「罪」的釋義。閱讀《 證主聖經神學辭典 》，請了解有哪三個因素使罪的悲劇變得複 雜?從而定下對付罪的方法和生活方式。</p>
+        </Col>
+      </Row>
+      <Row className="text-left mt-5" id="reaction-bar">
+        <Col className="form-inline" lg={{ offset: 2 }}>
+          <OverlayTrigger placement="auto" overlay={(props: any) => renderTooltip(props, '哈利路亞')}>
+            <div className="m-3" style={{ cursor: 'pointer' }} onClick={() => setReaction('hallelujah')}>
+              <div style={{ display: 'inline' }}>
+                <i className={`fas fa-hanukiah reaction ${hallelujah ? "reacted" : ""}`}></i>
+              </div>
+              <span style={{ fontSize: 24 }} className="m-1">0</span>
+            </div>
+          </OverlayTrigger>
+          <OverlayTrigger placement="auto" overlay={(props: any) => renderTooltip(props, '記念')}>
+            <div className="m-3" style={{ cursor: 'pointer' }} onClick={() => setReaction('pray')}>
+              <div style={{ display: 'inline' }}>
+                <i className={`fas fa-praying-hands reaction ${pray ? "reacted" : ""}`}></i>
+              </div>
+              <span style={{ fontSize: 24 }} className="m-1">0</span>
+            </div>
+          </OverlayTrigger>
         </Col>
       </Row>
     </Container>
