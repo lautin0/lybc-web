6ee86140646f0d74b718b77bbcea482b0b41eb1f
lautin0 <byzantium01@gmail.com>
2020-12-12 20:46:07 +0800

    ✨ Paginator algorithm

--- src/views/worship/WorshipList.tsx
+++ src/views/worship/WorshipList.tsx
@@ -25,7 +25,7 @@ function WorshipList() {
   const { loading, data: worshipData } = useQuery<{ worships: Worship[] }>(GET_WORSHIPS)
 
   const onPageChanged = useCallback((page: number) => {
-    if (page > Math.ceil(data.length / pageSize) || page === 0)
+    if (page > totalPageNum || page === 0)
       return
     let array: Array<{ worshipId: string, date: moment.Moment, title: string, messenger: string }> = [];
     for (let i = (pageSize * page) - pageSize; i < pageSize * page; i++) {
@@ -34,10 +34,14 @@ function WorshipList() {
     setPageItems(array)
     setPageNumber(page)
   }, [data])
-  
+
   function onCellClicked(id: any) {
     history.push('/worship/' + id)
   };
+
+  const adjacency = 1;
+
+  const totalPageNum = Math.ceil(data.length / pageSize)
 
   let items = [];
   if (pageItems == null || pageItems.length === 0) {
@@ -48,18 +52,79 @@ function WorshipList() {
       </Pagination.Item>
     );
     items.push(<Pagination.Next key={4} />, <Pagination.Last key={5} />)
-  } else {
+  } else if (totalPageNum <= (5 + (2 * adjacency))) { //5 = start + end + 2 Ellipsis + middle (Enough to show all)
     items.push(<Pagination.First key={1} onClick={() => onPageChanged(1)} />,
       <Pagination.Prev key={2} onClick={() => onPageChanged(pageNumber - 1)} />)
-    for (let number = 1; number <= Math.ceil(data.length / pageSize); number++) {
+    for (let number = 1; number <= totalPageNum; number++) {
       items.push(
         <Pagination.Item key={number + 2} active={number === pageNumber} onClick={() => onPageChanged(number)}>
           {number}
         </Pagination.Item>,
       );
     }
-    items.push(<Pagination.Next key={Math.ceil(data.length / pageSize) + 3} onClick={() => onPageChanged(pageNumber + 1)} />,
-      <Pagination.Last key={Math.ceil(data.length / pageSize) + 4} onClick={() => onPageChanged(Math.ceil(data.length / pageSize))} />)
+    items.push(<Pagination.Next key={totalPageNum + 3} onClick={() => onPageChanged(pageNumber + 1)} />,
+      <Pagination.Last key={totalPageNum + 4} onClick={() => onPageChanged(totalPageNum)} />)
+  } else {
+    items.push(<Pagination.First className="d-none d-sm-block" key={1} onClick={() => onPageChanged(1)} />,
+      <Pagination.Prev key={2} onClick={() => onPageChanged(pageNumber - 1)} />)
+
+    if (pageNumber <= 2 + (adjacency * 2)) {
+      // Close to start, hide close to end
+      for (let i = 1; i <= 2 + (adjacency * 2); i++) {
+        items.push(
+          <Pagination.Item key={i + 2} active={i === pageNumber} onClick={() => onPageChanged(i)}>
+            {i}
+          </Pagination.Item>,
+        )
+      }
+      items.push(<Pagination.Ellipsis key={totalPageNum + 2} />)
+      items.push(
+        <Pagination.Item active={totalPageNum === pageNumber} key={totalPageNum + 3} onClick={() => onPageChanged(totalPageNum)}>
+          {totalPageNum}
+        </Pagination.Item>,
+      )
+    } else if(pageNumber > (totalPageNum - ((adjacency * 2) + 2))){
+      // Close to end, hide close to start
+      items.push(
+        <Pagination.Item active={1 === pageNumber} key={3} onClick={() => onPageChanged(1)}>
+          {1}
+        </Pagination.Item>,
+      )
+      items.push(<Pagination.Ellipsis key={4} />)
+      for (let i = (totalPageNum - ((adjacency * 2) + 2)); i <= totalPageNum; i++) {
+        items.push(
+          <Pagination.Item key={i + 4} active={i === pageNumber} onClick={() => onPageChanged(i)}>
+            {i}
+          </Pagination.Item>,
+        )
+      }
+    } else {
+      // Handle middle, hide close to start and end
+      items.push(
+        <Pagination.Item active={totalPageNum === pageNumber} key={3} onClick={() => onPageChanged(1)}>
+          {1}
+        </Pagination.Item>,
+      )
+      items.push(<Pagination.Ellipsis key={4} />)
+
+      for (let i = pageNumber - adjacency; i <= pageNumber + adjacency; i++) {
+        items.push(
+          <Pagination.Item key={i + 4} active={i === pageNumber} onClick={() => onPageChanged(i)}>
+            {i}
+          </Pagination.Item>,
+        )
+      }
+
+      items.push(<Pagination.Ellipsis key={totalPageNum + 5} />)
+      items.push(
+        <Pagination.Item active={totalPageNum === pageNumber} key={totalPageNum + 6} onClick={() => onPageChanged(totalPageNum)}>
+          {totalPageNum}
+        </Pagination.Item>,
+      )
+    }
+
+    items.push(<Pagination.Next key={totalPageNum + 7} onClick={() => onPageChanged(pageNumber + 1)} />,
+      <Pagination.Last className="d-none d-sm-block" key={totalPageNum + 8} onClick={() => onPageChanged(totalPageNum)} />)
   }
 
   useEffect(() => {
@@ -92,7 +157,7 @@ function WorshipList() {
 
   useEffect(() => {
     document.title = "網上崇拜"
-  },[])
+  }, [])
 
   useEffect(() => {
     //Default scroll to top
