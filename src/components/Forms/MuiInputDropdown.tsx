7c4826264ca0644735620c381c0843e55d8fdf35
lautin0 <byzantium01@gmail.com>
2021-04-28 21:38:56 +0800

    🛠 Changed to Mui components in admin forms

---
+++ src/components/Forms/MuiInputDropdown.tsx
@@ -0,0 +1,57 @@
+import { InputLabel, Select, MenuItem, FormControl, FormHelperText, Grid, createStyles, makeStyles } from '@material-ui/core'
+import React from 'react'
+import { Controller, useFormContext } from 'react-hook-form'
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    item: {
+      paddingRight: theme.spacing(3),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      whiteSpace: 'nowrap',
+      marginBottom: theme.spacing(1),
+    },
+  }),
+);
+
+export default function MuiInputDropdown(props: any) {
+
+  const { name, label, isReadOnly, ds, md, sm, xs, skipValidate, placeholder, validateFn, strongReadOnly } = props;
+
+  const { errors, control } = useFormContext()
+
+  const classes = useStyles()
+
+  return <Grid
+    item
+    md={md}
+    xs={xs}
+    className={classes.item}
+  >
+    <Controller
+      name={name}
+      control={control}
+      rules={{ validate: validateFn }}
+      defaultValue=""
+      render={({ onChange, onBlur, value }) => {
+        return <FormControl variant="filled" fullWidth={true} error={!skipValidate && !!errors[name]}>
+          <InputLabel id="demo-simple-select-filled-label">{label}</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={value}
+            onChange={onChange}
+          >
+            {/* <MenuItem value="">
+              <em>None</em>
+            </MenuItem> */}
+            {ds.map((item: any, idx: number) => {
+              return <MenuItem key={idx} disabled={item.disabled} value={item.value}>{item.display}</MenuItem>
+            })}
+          </Select>
+          {(!skipValidate && errors[name]) && <FormHelperText>必須輸入這欄</FormHelperText>}
+        </FormControl>
+      }}
+    />
+  </Grid>
+}
--- src/components/Forms/MuiInputText.tsx
+++ src/components/Forms/MuiInputText.tsx
@@ -1,38 +1,56 @@
-import { Box, TextField } from '@material-ui/core';
+import { createStyles, Grid, makeStyles, TextField } from '@material-ui/core';
 import { RBRef } from 'adapter/types';
 import React from 'react'
 import { Controller, useFormContext } from 'react-hook-form';
 
+const useStyles = makeStyles((theme) =>
+   createStyles({
+      item: {
+         paddingRight: theme.spacing(3),
+         textAlign: 'center',
+         color: theme.palette.text.secondary,
+         whiteSpace: 'nowrap',
+         marginBottom: theme.spacing(1),
+      },
+   }),
+);
+
 function MuiInputText(props: any) {
 
-   const { name, label, isReadOnly, md, sm, skipValidate, placeholder, validateFn, strongReadOnly, width } = props;
+   const { name, label, isReadOnly, md, sm, skipValidate, placeholder, validateFn, strongReadOnly } = props;
 
    const { errors, control } = useFormContext()
 
-   return <Box
-      width={width}
+   const classes = useStyles()
+
+   return <Grid
+      item
+      md={md}
+      sm={sm}
+      className={classes.item}
    >
       <Controller
          name={name}
          control={control}
+         defaultValue=""
          rules={{ validate: validateFn }}
          render={({ onChange, onBlur, value }) => {
             return <TextField
                error={!skipValidate && !!errors[name]}
                // id="standard-error-helper-text"  
                placeholder={placeholder}
-               fullWidth={true}    
+               fullWidth={true}
                label={label}
                variant="outlined"
                onChange={onChange}
                onBlur={onBlur}
-               defaultValue={value}
+               value={value}
                disabled={strongReadOnly || isReadOnly}
                helperText={(!skipValidate && errors[name]) && <label style={{ opacity: .6, color: '#FF3636' }}>必須輸入這欄</label>}
             />
          }}
       />
-   </Box>
+   </Grid>
 }
 
 export default MuiInputText;
