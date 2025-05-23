5904d2c11293415bf793c8d680bef21ac99f15e0
lautin0 <byzantium01@gmail.com>
2025-05-22 22:17:32 -0400

    misc: layout update

--- src/views/index-sections/Carousel.tsx
+++ src/views/index-sections/Carousel.tsx
@@ -21,10 +21,6 @@ function CarouselSection() {
   const [index, setIndex] = useState(0);
   const [setClickFunc] = useState<any>(() => { })
 
-  // const [votd, setVotd] = useState<string>()
-  // const[votdImg, setVotdImg] = useState<string>()
-  // const[votdSrc, setVotdSrc] = useState<string>()
-
   const { data, loading } = useMaxWorshipIdQuery()
 
   const handleSelect = (selectedIndex: number) => {
@@ -37,7 +33,6 @@ function CarouselSection() {
 
   useEffect(() => {
     if (data !== undefined) {
-      // dispatch(setLoading(false))
       setWorshipId(data?.maxWorshipId.toString())
     }
   }, [data])
@@ -48,40 +43,12 @@ function CarouselSection() {
     worshipId.length > 0 && setClickFunc(() => handleClick)
   }, [worshipId, setClickFunc, handleClick])
 
-  // useEffect(() => {
-  //   axios.get("https://www.bible.com/verse-of-the-day").then((res) => {
-  //     var parser = new DOMParser();
-  //     var htmlDoc = parser.parseFromString(res.data, 'text/html');
-  //     let imgs = htmlDoc.getElementsByTagName("amp-img")
-  //     for (let i = 0; i < imgs.length; i++) {
-  //       let img = imgs.item(i)
-  //       if (img != null && img.classList.contains("img-wrap")) {
-  //         let uri = img.getAttribute("src");
-  //         if (uri != null) {
-  //           let trimKey = "https://s3.amazonaws.com/"
-  //           uri = uri?.substring(uri.indexOf(trimKey), uri.length + trimKey.length)
-  //           console.log(uri)
-  //           setVotdImg(uri)
-  //         }
-  //       }
-  //     }
-  //   })
-  //   axios.get("https://www.bible.com/zh-HK/verse-of-the-day").then((res) => {
-  //     var parser = new DOMParser();
-  //     var htmlDoc = parser.parseFromString(res.data, 'text/html');
-  //     let votdHtml = htmlDoc.getElementsByClassName("near-black mt0 mb2")[0]
-  //     setVotd(votdHtml.innerHTML)
-  //     let votdSrcHtml = htmlDoc.getElementsByClassName("usfm fw7 mt0 mb0 gray f7 ttu")[0]
-  //     setVotdSrc(votdSrcHtml.innerHTML)
-  //   })
-  // }, [])
-
   return (
     <>
       <div className="section" id="carousel" style={{ marginTop: 50, marginBottom: 50 }}>
         <Container>
           <Row style={{ marginRight: 0, marginLeft: 0 }}>
-            <Col lg={6} sm={12} className="mb-5">
+            <Col className="mb-5">
               <h3><b className="pl-3" style={{ borderLeft: 'solid .5rem #FFB236' }}>{intl.formatMessage({ id: 'app.latest-updates' })}</b></h3>
               {loading && <div
                 className="wrapper-cell"
@@ -105,34 +72,6 @@ function CarouselSection() {
                   <hr style={{ width: '80%' }}></hr>
                 </div>
               ))}
-            </Col>
-            <Col lg={6} sm={12}>
-              {!loading &&
-                <Carousel activeIndex={index} onSelect={handleSelect} style={{ cursor: "pointer" }}>
-                  <Carousel.Item style={{ background: 'lightgray' }}>
-                    <img
-                      style={{ width: '100%', objectFit: "cover" }}
-                      src={UNIVERSALS.GOOGLE_STORAGE_ENDPOINT + "/lybcstorage/togetherness_md.jpg"}
-                      alt="Second slide"
-                    />
-
-                    <Carousel.Caption style={{
-                      background: 'rgba(100, 100, 100, .5)'
-                    }}>
-                      <Button style={{ color: 'white', textDecoration: 'none' }} className="btn-link">
-                        {window.innerWidth > 991 && <><h4>{intl.formatMessage({ id: "app.theme.title" })}</h4>
-                          <h3>{intl.formatMessage({ id: "app.theme.l1" })}</h3>
-                          <h3>{intl.formatMessage({ id: "app.theme.l2" })}</h3>
-                        </>}
-                        {window.innerWidth <= 991 && <><h5>{intl.formatMessage({ id: "app.theme.title" })}</h5>
-                          <h4 style={{ marginTop: 0 }}>{intl.formatMessage({ id: "app.theme.l1" })}</h4>
-                          <h4 style={{ marginTop: 0 }}>{intl.formatMessage({ id: "app.theme.l2" })}</h4>
-                        </>}
-                      </Button>
-                    </Carousel.Caption>
-                  </Carousel.Item>
-                </Carousel>}
-              {loading && <LoadingDiv />}
             </Col>
           </Row>
         </Container>
